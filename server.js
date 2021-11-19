cf85bc3d8aa80df4626e3ddbb2397c026d8da166
unknown <0202alcc@gmail.com>
2021-11-18 18:49:42 -0800

    server setup

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+// Mapping the EJS template engine to ".html" files
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function(req,res) {
+ res.render(__dirname + '/home.html');
+})
+app.listen(8080);
+console.log('Express App is running on port 8080...');
